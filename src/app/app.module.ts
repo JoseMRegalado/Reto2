2e154dd38510ae8d32e5299c60414e36cf8968d8
JoseMRegalado <mjregalado2@utpl.edu.ec>
2025-01-06 20:37:16 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {RouterLink, RouterModule, RouterOutlet} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    RouterOutlet,
+    //RouterModule.forRoot(appRoutes),
+    RouterLink,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDHmfeK67rw_acO5sTQZP-rBVAntsPL_OU",
+    authDomain: "reto2-597e3.firebaseapp.com",
+    projectId: "reto2-597e3",
+    storageBucket: "reto2-597e3.firebasestorage.app",
+    messagingSenderId: "755648731240",
+    appId: "1:755648731240:web:c5c32ab28a3d5f7e2343dd"
+  }
+};
